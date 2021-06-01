6effedf161d1189eac7d71e77cc4291d17b35887
avnish1981 <avnish.choubey@gmail.com>
2021-06-01 17:26:10 +0530

    Initial Checikns

---
+++ TennecoAPIMgmt/Controllers/VendorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TennecoAPIMgmt.Models;
+using TennecoAPIMgmt.Repositories;
+
+namespace TennecoAPIMgmt.Controllers
+{
+    public class VendorController : ApiController
+    {
+        vendorRepositories vendorRepositories = new vendorRepositories();
+        
+        public IHttpActionResult  GetAllVendors()
+        {
+            return Ok(vendorRepositories.GetAllVendors());
+        }
+
+        public IHttpActionResult  InsertVendor([FromBody]Vendor vendor)
+        {
+            if(vendor==null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                vendorRepositories.Insertvendor(vendor);
+                return Content(HttpStatusCode.Created, "new vendor inserted");
+            }
+            
+        }
+    }
+}
---
+++ TennecoAPIMgmt/Repositories/vendorRepositories.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TennecoAPIMgmt.Models;
+
+namespace TennecoAPIMgmt.Repositories
+{
+    public class vendorRepositories
+    {
+        ApplicationDbContext entities = new ApplicationDbContext();
+
+        public List<Vendor > GetAllVendors()
+        {
+            return entities.Vendors.ToList();
+        }
+        public void Insertvendor(Vendor vendor )
+        {
+            entities.Vendors.Add(vendor);
+            entities.SaveChanges();
+        }
+    }
+}
