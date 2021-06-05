741f2b24ac33f0eb9ce5eefb25d7e7463ad168a5
avnish1981 <avnish.choubey@gmail.com>
2021-06-05 13:41:16 +0530

    aading

--- TennecoAPIMgmt/Repositories/employeeRepository.cs
+++ TennecoAPIMgmt/Repositories/employeeRepository.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using TennecoAPIMgmt.Models;
 
 namespace TennecoAPIMgmt.Repositories
 {
     public class employeeRepository
     {
-       
+        ApplicationDbContext entities = new ApplicationDbContext();
+        public List<Employee > GetAllfactories()
+        {
+            return entities.Employees .ToList();
+        }
+        public Employee GetfactoryByID(int id)
+        {
+            Employee factory = entities.Employees .FirstOrDefault(a => a.Id  == id);
+            return factory;
+        }
+        public void Insertfactory(Employee employee)
+        {
+            entities.Employees.Add(employee);
+            entities.SaveChanges();
+        }
+
     }
 }
